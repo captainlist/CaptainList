8cd28e20ec7e7cbf7050fa7de794695adec5b8b4
abhishekpatnia <abhishekpatnia@gmail.com>
2014-02-12 19:46:21 -0800

    Initial Commit

---
+++ CaptainList/SimOrgAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimOrgAppDelegate.h
+//  CaptainList
+//
+//  Created by abhishek patnia on 2/12/14.
+//  Copyright (c) 2014 SimOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimOrgAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CaptainList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CaptainList
+//
+//  Created by abhishek patnia on 2/12/14.
+//  Copyright (c) 2014 SimOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimOrgAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimOrgAppDelegate class]));
+    }
+}
