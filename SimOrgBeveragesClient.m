940c907693a6d0fc3dd2102b37b0ff180231afb4
abhishekpatnia <abhishekpatnia@gmail.com>
2014-02-15 22:41:24 -0800

    Removing auth keys..keep it local

--- SimOrgBeveragesClient.m
+++ SimOrgBeveragesClient.m
@@ -7,10 +7,42 @@
 //
 
 #import "SimOrgBeveragesClient.h"
+#import "ASAPIClient.h"
+#import "Beverage.h"
+
+@interface SimOrgBeveragesClient ()
+@property ASRemoteIndex *landingPageIndex;
+@property ASRemoteIndex *fullIndex;
+@end
 
 @implementation SimOrgBeveragesClient
-- (NSArray *)getBeveragesWithQuery:(NSDictionary *)beverageQuery yelp:(NSArray *)yelpQuery {
-    return nil;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        ASAPIClient *apiClient =
+        
+        self.landingPageIndex = [apiClient getIndex:@"landing_beverages"];
+        self.fullIndex = [apiClient getIndex:@"beverages"];
+    }
+    return self;
+}
+
+- (NSArray *)getLandingPageBeveragesWithQuery:(NSString *)beverageQuery yelp:(NSArray *)yelpQuery beverages:(NSMutableArray **) beverages {
+    ASQuery* query = [ASQuery queryWithFullTextQuery:beverageQuery];
+    [self.landingPageIndex search:query success:^(ASRemoteIndex *index, ASQuery *query, NSDictionary *result) {
+        for (id hit in [result objectForKey:@"hits"]) {
+            Beverage *beverage = [[Beverage alloc] init];
+            beverage.Name = [hit objectForKey:@"Name"];
+            NSLog(beverage.Name);
+            [*beverages addObject:beverage];
+        }
+    } failure:^(ASRemoteIndex *index, ASQuery *query, NSString *errorMessage) {
+        NSLog(errorMessage);
+    }];
+    
+    return *beverages;
 }
 
 @end
